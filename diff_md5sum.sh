dd03ab2463b79765c5e1f3baca436a77bbf41008
nkiyo <nkiyo@hoge.com>
2019-05-11 07:39:54 +0900

    up

--- diff_md5sum.sh
+++ diff_md5sum.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# 特定ディレクトリに移動してコマンド実行？
-# md5sum 区切り文字をcommaに。ファイル名を行頭に
 
 # get current md5sum
+OLDIFS=$IFS
+IFS="\n"
 current=$(cd ./post ; md5sum ./*.txt)
-echo "## current =>" $current
-echo
+for line in $current; do
+  # md5sum 区切り文字をcommaに。ファイル名を行頭に
+  echo $line
+done
+IFS=$OLDIFS
 
 # diff prev to current
+OLDIFS=$IFS
+IFS="\n"
 prev=$(cd ./pre ; md5sum ./*.txt)
-echo "## prev =>" $prev
+for line in $pre; do
+  # md5sum 区切り文字をcommaに。ファイル名を行頭に
+  echo $line
+done
+IFS=$OLDIFS
 
 # create md5sum.txt for next interval
 
